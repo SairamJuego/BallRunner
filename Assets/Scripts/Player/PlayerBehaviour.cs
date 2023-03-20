abc3a6a3ee68444a37b34bf13a07faad466ed513
Sairam <sairamjuego@gmail.com>
2023-03-20 16:18:42 +0530

    Added Jump to player and Camera follow script

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -4,39 +4,61 @@ using UnityEngine;
 
 namespace BR.Player
 {
-public class PlayerBehaviour : MonoBehaviour
-{
-    #region Variables
-    //private variables
-    Rigidbody rb;
-    Vector3 move = Vector3.zero;
-
-    //private serializable variables
-    [SerializeField] private float moveSpeed;
-
-    #endregion
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerBehaviour : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-    }
+        #region Variables
+        //private variables
+        Rigidbody rb;
+        //Vector3 move = Vector3.zero;
+        private bool _isGrounded = false;
 
-    // Update is called once per frame
-    void Update()
-    {
+        //private serializable variables
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _jumpSpeed;
+
+        
+
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            {
+                rb.AddForce(Vector3.up * _jumpSpeed , ForceMode.Impulse);
+                _isGrounded = false;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            //for moving
+            rb.AddForce(Movement() * _moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        Vector3 Movement()
+        {
+            return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        }
+
+        void OnCollisionEnter(Collision collisionInfo)
+        {
+            _isGrounded = true;   
+        }
+        void OnCollisionExit(Collision collisionInfo)
+        {
+            _isGrounded = false;
+        }
+
+        
         
     }
-
-    void FixedUpdate()
-    {
-        rb.AddForce(Movement()*moveSpeed*Time.deltaTime,ForceMode.Impulse);
-    }
-
-    Vector3 Movement()
-    {
-        return move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-    }
-}
 
 }
